6cd2f7ce8902aaa8a901f0bd92f7a8f2cf852f6f
Pablo Narvaja <pnarvaja.21@gmail.com>
2018-11-01 22:55:58 -0300

    Cleaning

--- EJ17_RuntimeDatatypes/EJ17_StaticDef.cpp
+++
@@ -1,8 +0,0 @@
-#include "EJ17_RuntimeDatatypes.hpp"
-
-uint32 ComponentManager::componentsCounts = 0;
-
-template<class T>
-uint32 BaseComponentMetadata<T>::ID = ComponentManager::componentsCounts++;
-template<class T>
-size_t BaseComponentMetadata<T>::SIZE = sizeof(T);
